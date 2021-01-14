78faeedbec356487314ff14a267a52ed6e97b9f5
Limm-jk <201602057@cs-cnu.org>
2021-01-14 17:34:19 +0900

    cost문제

--- 1. 재귀적 프로그래밍/cost.py
+++ 1. 재귀적 프로그래밍/cost.py
@@ -1,6 +1,7 @@
 bill = [1,2,5,10,20,50]
 
 def rec(bill, money:int):
+    # default case
     if len(bill) == 1:
         if money % bill[0] == 0:
             return 1
@@ -8,6 +9,7 @@ def rec(bill, money:int):
             return 0
     curr = bill.pop()
     count = 0
+    # ???
     while money > curr:
         count += rec(bill, money)
         money -= curr
